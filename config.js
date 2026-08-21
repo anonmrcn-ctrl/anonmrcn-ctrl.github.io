ca01a93822719e54fb1dcebeffe57b8010850476
anonmrcn-ctrl <anonmrcn@gmail.com>
2026-08-21 17:53:36 +0200

    Aggiungi configurazione API

---
+++ config.js
@@ -0,0 +1,6 @@
+/*
+ * Inserire qui l'URL pubblico del Cloudflare Worker dopo il deploy.
+ * Esempio:
+ * window.NNMRCN_API_BASE = "https://nnmrcn-rete.nome-account.workers.dev";
+ */
+window.NNMRCN_API_BASE = "";
