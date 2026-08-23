05e4dfa69f6928bb3a8da200906d7374d29d53e9
anonmrcn-ctrl <anonmrcn@gmail.com>
2026-08-23 10:17:24 +0200

    Collega i paesaggi pubblici alla mappa

--- config.js
+++ config.js
@@ -4,3 +4,23 @@
  * window.NNMRCN_API_BASE = "https://nnmrcn-rete.nome-account.workers.dev";
  */
 window.NNMRCN_API_BASE = "";
+
+/*
+ * Rende disponibile la mappa Leaflet anche ai livelli pubblici
+ * caricati separatamente da accesso.js.
+ */
+if (window.L && typeof L.map === "function") {
+    const creaMappaLeaflet = L.map.bind(L);
+
+    L.map = function(...args) {
+        const instance = creaMappaLeaflet(...args);
+        window.NNMRCN_MAP = instance;
+        return instance;
+    };
+}
+
+window.addEventListener("load", () => {
+    const script = document.createElement("script");
+    script.src = "./paesaggi.js";
+    document.body.appendChild(script);
+});
